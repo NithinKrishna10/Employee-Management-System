f1042bc8d1212fc91d818f56d2dfce8000685878
nithin3119@gmail.com <nithin3119@gmail.com>
2024-10-26 00:44:59 +0530

    update

--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 
 from employee.models import Employee, EmployeeField
@@ -21,13 +21,31 @@ def dashboard(request):
     return render(request, 'dashboard.html')
 
 
+
 def settings_view(request):
     fields = EmployeeField.objects.all()
+
     if request.method == 'POST':
         field_name = request.POST.get('field_name')
         EmployeeField.objects.create(name=field_name)
-        return redirect('employee_list')
+        return redirect('settings')
+
     return render(request, 'settings.html', {'fields': fields})
+
+def edit_field_view(request, field_id):
+    field = get_object_or_404(EmployeeField, id=field_id)
+
+    if request.method == 'POST':
+        new_name = request.POST.get('new_name')
+        field.name = new_name
+        field.save()
+        return redirect('settings')
+def delete_field_view(request, field_id):
+    field = get_object_or_404(EmployeeField, id=field_id)
+
+    if request.method == 'POST':
+        field.delete()
+        return redirect('settings')
 
 
 def employee_list(request):
@@ -37,6 +55,18 @@ def employee_list(request):
         fields = EmployeeField.objects.all()
     except:
         fields = []
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        
+        # Collect data for dynamic fields
+        additional_data = {field.name: request.POST.get(field.name) for field in fields}
+        print(name,email,phone,additional_data,'-----------')
+        # Create the new employee with additional data
+        Employee.objects.create(name=name, email=email, phone=phone, additional_data=additional_data)
+        return redirect('employee_list')
+    
     return render(request, 'employee_list.html', {'employees': employees, 'fields': fields})
 
 
@@ -53,7 +83,7 @@ def employee_create(request):
         
         # Collect data for dynamic fields
         additional_data = {field.name: request.POST.get(field.name) for field in fields}
-        
+        print(name,email,phone,additional_data,'-----------')
         # Create the new employee with additional data
         Employee.objects.create(name=name, email=email, phone=phone, additional_data=additional_data)
         return redirect('employee_list')
